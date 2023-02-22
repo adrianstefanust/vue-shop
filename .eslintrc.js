9141e5c091c44832f34bdd1996396ae62c92c8e5
adrianstefanust <astefanus44@gmail.com>
2023-02-22 09:57:25 +0700

    add shop page

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,7 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    'plugin:prettier/recommended'
-  ],
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', 'plugin:prettier/recommended'],
   parserOptions: {
     parser: '@babel/eslint-parser'
   },
@@ -16,6 +12,7 @@ module.exports = {
     'vue/no-multiple-template-root': 'off',
     'no-unused-vars': 'off',
     'no-use-before-define': 'off',
+    'vue/multi-word-component-names': 0,
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
