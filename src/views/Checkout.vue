d5621f010cadc81909f8e48ab031bd08fdc89e3c
adrianstefanust <astefanus44@gmail.com>
2023-02-24 08:51:18 +0700

    edit checkout page

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -1,52 +1,57 @@
 <template>
     <div class="container">
-      <h1>Checkout</h1>
+        <h1>Checkout</h1>
+
+        <table class="table table-hover" v-if="cart.length">
+            <caption class="text-right h3">
+                <b>Total:</b>
+                <curr :amt="Number(cartTotal)"></curr>
+            </caption>
+            <thead>
+                <tr>
+                    <th scope="col"></th>
+                    <th scope="col">Item</th>
+                    <th scope="col" class="text-center">Qty</th>
+                    <th scope="col" class="text-right">Price</th>
+                    <th scope="col" class="text-right">Sub-total</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(item,index) in cart" :key="item.product.id">
+                    <td class="text-center">
+                        <div class="btn-group" role="group" aria-label="Basic example">
+                            <button @click="this.$emit('addItem', item.product)" class="btn btn-success">
+                                +
+                            </button>
+                            <button @click="this.$emit('deleteItem', index)" class="btn btn-outline-success">
+                                -
+                            </button>
+                        </div>
+                    </td>
+                    <th scope="row">{{item.product.name}}</th>
+                    <td class="text-center">{{item.qty}}</td>
+                    <td class="text-right">
+                        <curr :amt="Number(item.product.price)"></curr>
+                    </td>
+                    <td class="text-right">
+                        <curr :amt="item.qty * Number(item.product.price)"></curr>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <router-link class="btn btn-sm btn-success" to="/">Keep Shopping</router-link>
+    </div>
+</template>
   
-      <table class="table table-hover">
-        <caption class="text-right h3">
-          <b>Total:</b>
-          Total
-        </caption>
-        <thead>
-          <tr>
-            <th scope="col"></th>
-            <th scope="col">Item</th>
-            <th scope="col" class="text-center">Qty</th>
-            <th scope="col" class="text-right">Price</th>
-            <th scope="col" class="text-right">Sub-total</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td class="text-center">
-              <div class="btn-group" role="group" aria-label="Basic example">
-                <button class="btn btn-success">
-                  +
-                </button>
-                <button class="btn btn-outline-success">
-                  -
-                </button>
-              </div>
-            </td>
-            <th scope="row">item name</th>
-            <td class="text-center">item qty</td>
-            <td class="text-right">
-              price
-            </td>
-            <td class="text-right">
-              subtotal
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      <router-link class="btn btn-sm btn-success" to="/"
-        >Keep Shopping</router-link
-      >
-    </div></template
-  >
+<script>
+import Curr from '@/components/Curr'
+export default {
+    props: ['cart', 'cartTotal'],
+    components: {
+        Curr
+    },
+    emits: ['addItem', 'deleteItem']
+}
+</script>
   
-  <script>
-  export default {}
-  </script>
-  
-  <style></style>
+<style></style>
